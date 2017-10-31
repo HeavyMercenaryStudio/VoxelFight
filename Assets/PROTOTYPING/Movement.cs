106b4fc9de66b10e5c2390062c6a524c47c87707
Eram <pm4281@gmail.com>
2017-10-31 19:04:35 +0100

    PlaceHolder

--- Assets/PROTOTYPING/Movement.cs
+++ Assets/PROTOTYPING/Movement.cs
@@ -5,26 +5,67 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Movement : MonoBehaviour {
 
+   [SerializeField] float movementSpeed = 10f;
+   [SerializeField] float rotationSpeed = 100f;
+
     Rigidbody rigibody;
+
+    [SerializeField] bool alternativeMovement = false;
+
+    private void Start()
+    {
+        rigibody = GetComponent<Rigidbody> ();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Move (h, v);
+
+        float h = Input.GetAxis ("Horizontal");
+        float v = Input.GetAxis ("Vertical");
+
+        Move (h, v);
+
     }
 
     void Move(float horizontal, float vertical)
     {
-        Vector3 rotationVector = new Vector3 (0, horizontal, 0);
-        //rotationVector *= Time.deltaTime * maxRotationSpeed;
 
-        Quaternion deltaRotation = Quaternion.Euler (rotationVector);
-        rigibody.MoveRotation (rigibody.rotation * deltaRotation);
+        if (alternativeMovement)
+        {
+            Vector3 rotationVector = new Vector3 (0, horizontal, 0);
+            rotationVector *= Time.deltaTime * rotationSpeed;
 
-        Vector3 moveVector = (vertical) * transform.forward;
-        //moveVector *= Time.deltaTime * maxCarSpeed;
+            Quaternion deltaRotation = Quaternion.Euler (rotationVector);
+            rigibody.MoveRotation (rigibody.rotation * deltaRotation);
 
-        moveVector += transform.position;
-        rigibody.MovePosition (moveVector);
+            Vector3 moveVector = (vertical) * transform.forward;
+            moveVector *= Time.deltaTime * movementSpeed;
+
+            moveVector += transform.position;
+            rigibody.MovePosition (moveVector);
+        }
+        else
+        {
+            Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast (camRay, out hit))
+            {
+                Vector3 playerToMouse = hit.point - transform.position;
+                playerToMouse.y = 0f;
+                Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+                rigibody.MoveRotation (newRotation);
+
+                Vector3 moveVector = new Vector3(horizontal,0,vertical);
+                moveVector *= Time.deltaTime * movementSpeed;
+                moveVector += transform.position;
+                rigibody.MovePosition (moveVector);
+
+            }
+        }
     }
+
+
+
 }
