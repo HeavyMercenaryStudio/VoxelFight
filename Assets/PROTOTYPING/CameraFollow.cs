694cb8bc4e5de45086b34ebf13cc24fd046554e3
Eram <pm4281@gmail.com>
2017-10-31 18:28:14 +0100

    Initial Commit

---
+++ Assets/PROTOTYPING/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.CameraUI
+{
+	public class CameraFollow : MonoBehaviour {
+
+		[SerializeField] bool followPlayer = false; // may I follow player ?
+
+        private GameObject player; // Reference to player
+
+		void Start(){
+			player = GameObject.FindGameObjectWithTag ("Player"); // Find player
+		}
+
+		void Update(){
+
+            if (followPlayer)
+				this.transform.position = player.transform.position;
+
+		}
+	}
+
+}
---
+++ Assets/PROTOTYPING/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour {
+
+    Rigidbody rigibody;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Move (h, v);
+    }
+
+    void Move(float horizontal, float vertical)
+    {
+        Vector3 rotationVector = new Vector3 (0, horizontal, 0);
+        //rotationVector *= Time.deltaTime * maxRotationSpeed;
+
+        Quaternion deltaRotation = Quaternion.Euler (rotationVector);
+        rigibody.MoveRotation (rigibody.rotation * deltaRotation);
+
+        Vector3 moveVector = (vertical) * transform.forward;
+        //moveVector *= Time.deltaTime * maxCarSpeed;
+
+        moveVector += transform.position;
+        rigibody.MovePosition (moveVector);
+    }
+}
