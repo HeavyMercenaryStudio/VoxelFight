12696556658c21fc5d2f074b85d34a0eedbd6f4f
Eram <pm4281@gmail.com>
2017-11-02 15:38:00 +0100

    NIC

---
+++ Assets/_Weapons/Weapon.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : ScriptableObject {
+
+    [SerializeField] GameObject weaponPrefab;
+
+    [SerializeField] float weaponDamage;
+    [SerializeField] float weaponMaxAmmo;
+    [SerializeField] float weaponAmmo;
+    [SerializeField] float weaponRange;
+
+
+    #region Getters
+    public float GetWeaponDamage(){
+        return weaponDamage;
+    }
+    public float GetWeaponAmmo(){
+        return weaponAmmo;
+    }
+    public float GetWeaponMaxAmmo(){
+        return weaponMaxAmmo;
+    }
+    public float GetWeaponRange(){
+        return weaponRange;
+    }
+    #endregion
+    #region Setters
+    public void SetWeaponDamage(float value){
+         weaponDamage = value;
+    }
+    public void SetWeaponAmmo(float value){
+         weaponAmmo = value;
+    }
+    public void SetWeaponMaxAmmo(float value){
+         weaponMaxAmmo = value;
+    }
+    public void SetWeaponRange(float value){
+         weaponRange = value;
+    }
+    #endregion
+
+}
